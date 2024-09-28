8e308ae21610837cb986cb0e579da69c715d04b2
rishav_learnerml <rishavbio1@gmail.com>
2024-09-28 15:38:32 +0530

    favicon issue resolved

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -1,25 +1,32 @@
 import { Button } from "./button";
+import Image from "next/image";
+import logo from "../assets/indipe_logo.png"
 
 interface AppbarProps {
-    user?: {
-        name?: string | null;
-    },
-    // TODO: can u figure out what the type should be here?
-    onSignin: any,
-    onSignout: any
+  user?: {
+    name?: string | null;
+  };
+  // TODO: can u figure out what the type should be here?
+  onSignin: any;
+  onSignout: any;
 }
 
-export const Appbar = ({
-    user,
-    onSignin,
-    onSignout
-}: AppbarProps) => {
-    return <div className="flex justify-between border-b px-4">
-        <div className="text-lg flex flex-col justify-center">
-            PayTM
-        </div>
-        <div className="flex flex-col justify-center pt-2">
-            <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
-        </div>
+export const Appbar = ({ user, onSignin, onSignout }: AppbarProps) => {
+  return (
+    <div className="flex justify-between border-b px-4">
+      <div className="text-lg flex flex-col justify-center">
+        <Image
+          src={logo} // Replace with your logo image path
+          alt="Your App Logo"
+          width={70}
+          height={50}
+        />
+      </div>
+      <div className="flex flex-col justify-center pt-2">
+        <Button onClick={user ? onSignout : onSignin}>
+          {user ? "Logout" : "Login"}
+        </Button>
+      </div>
     </div>
-}
+  );
+};
